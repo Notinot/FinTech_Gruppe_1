c9d90ed50d15332eedd7cfd10bc1f3660a85b9af
Notinot <75838227+Notinot@users.noreply.github.com>
2023-11-02 16:03:39 +0100

    Update registration_screen.dart

--- lib/Screens/registration_screen.dart
+++ lib/Screens/registration_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'login_screen.dart';
 import 'package:http/http.dart' as http;
 
 class RegistrationScreen extends StatefulWidget {

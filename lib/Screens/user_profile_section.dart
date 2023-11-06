0c6a4727798dec4bb44c59d32337b3a08fdd6e4a
Notinot <labinot-cekaj@outlook.com>
2023-11-07 00:16:51 +0100

    removed dashboard changes for now

--- lib/Screens/user_profile_section.dart
+++ lib/Screens/user_profile_section.dart
@@ -5,25 +5,20 @@ import 'package:flutter/material.dart';
 
 class UserProfileSection extends StatelessWidget {
   final String username;
-  final Map<String, dynamic> picture;
 
-  const UserProfileSection(this.username, this.picture, {super.key});
+  const UserProfileSection(this.username, {super.key});
 
   @override
   Widget build(BuildContext context) {
     Image image;
-    if (picture != null && picture.containsKey("imageData")) {
-      final Uint8List imageBytes = base64Decode(picture["imageData"]);
-      final ImageProvider<Object> userImage = MemoryImage(imageBytes);
-      image = userImage as Image;
-    } else {
-      // Use a fallback image if the image data is not available in the JSON data
-      image = Image.asset(
-        "lib/assets/profile_image.png",
-        width: 40,
-        height: 40,
-      );
-    }
+
+    // Use a fallback image if the image data is not available in the JSON data
+    image = Image.asset(
+      "lib/assets/profile_image.png",
+      width: 40,
+      height: 40,
+    );
+
     return Column(
       children: <Widget>[
         CircleAvatar(

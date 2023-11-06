6fcd6f9d7f2ab6100e9730cc3ba2a580ea39c47c
lumein <50828237+lumein@users.noreply.github.com>
2023-11-06 13:33:13 +0100

    Server health check on Startup - display error if server is unavailable

--- lib/main.dart
+++ lib/main.dart
@@ -3,33 +3,21 @@ import 'Screens/login_screen.dart';
 import 'package:http/http.dart' as http;
 
 void main() async {
-  // Check server availability
-  // const serverUrl = 'http://192.168.2.33:3000';
-  const serverUrl = 'http://localhost:3000';
-  final response = await http.get(Uri.parse('$serverUrl/health'));
-
-  // Create the Flutter app
-  runApp(PayfriendzApp(serverAvailable: response.statusCode == 200));
+  runApp(PayfriendzApp());
 }
 
 class PayfriendzApp extends StatelessWidget {
+  // Define a boolean variable to track server availability
   final bool serverAvailable;
 
-  const PayfriendzApp({super.key, required this.serverAvailable});
+  // Constructor for PayfriendzApp
+  const PayfriendzApp({super.key, this.serverAvailable = false});
 
   @override
   Widget build(BuildContext context) {
-    if (serverAvailable) {
-      // If the server is available, show the LoginScreen as the initial route
-      return const MaterialApp(
-        home: LoginScreen(),
-      );
-    } else {
-      // If the server is unavailable, show an error screen
-      return const MaterialApp(
-        home: ServerUnavailableScreen(),
-      );
-    }
+    return MaterialApp(
+      home: serverAvailable ? LoginScreen() : ServerUnavailableScreen(),
+    );
   }
 }
 
@@ -46,7 +34,6 @@ class ServerUnavailableScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Display a message for server unavailability
             const Text('The server is currently unavailable.'),
             ElevatedButton(
               onPressed: () {

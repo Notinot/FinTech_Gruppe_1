494e973974184dd998cc0716c0e38373be8627b6
lumein <50828237+lumein@users.noreply.github.com>
2023-12-15 14:49:07 +0100

    user can switch between light and dark theme via button

---
+++ lib/Screens/Dashboard/themeNotifier.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ThemeNotifier extends ChangeNotifier {
+  bool _darkTheme = false;
+
+  bool get darkTheme => _darkTheme;
+
+  set darkTheme(bool value) {
+    _darkTheme = value;
+    notifyListeners();
+  }
+}
